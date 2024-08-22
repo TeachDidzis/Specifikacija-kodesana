a840f69f224e8fda850f9df1bb0749d77fc0f8bd
TeachDidzis <155529790+TeachDidzis@users.noreply.github.com>
2024-08-22 15:08:15 +0300

    Update specifikacija2_kods.py

--- specifikacija2_kods.py
+++ specifikacija2_kods.py
@@ -2,20 +2,26 @@ print("Četrstūra laukuma aprēķināšana!")
 
 while True:
     try:
-        garums = float(input("Ievadiet četrstūra garumu (cm): "))
-        if garums <= 0:
-            print("Ievadīts negatīvs skaitlis!")
-            continue
-        platums = float(input("Ievadiet četrstūra platumu (cm): "))
-        if platums <= 0:
-            print("Ievadīts negatīvs skaitlis!")
-            continue
-        else:
-            laukums = garums * platums
-            print(f"Četrstūra laukums ir {laukums} cm2")
-            break
+        while True:
+            garums = float(input("Ievadiet četrstūra garumu (cm): "))
+            if garums <= 0:
+                print("Ievadīts negatīvs skaitlis!")
+                continue
+            else:
+                break
+        while True:
+            platums = float(input("Ievadiet četrstūra platumu (cm): "))
+            if platums <= 0:
+                print("Ievadīts negatīvs skaitlis!")
+                continue
+            else:
+                break
+        laukums = garums * platums
+        print(f"Četrstūra laukums ir {laukums} cm2")
+        break
     except ValueError:
         print("Nav ievadīts skaitlis!")
 
 print("\nPaldies par darbu!")
+
 
